7279e56a1849b7aad74173ca58a87c20f5a0cf23
John Oh <johnoh0317@gmail.com>
2024-08-26 14:38:58 -0700

    worked on logout feature

--- src/pages/Account/Logout.js
+++ src/pages/Account/Logout.js
@@ -1,29 +1,32 @@
-// import AccountNav from "./AccountNav";
-// import { Link, useNavigate } from "react-router-dom";
-// import React, { useContext, useState } from "react";
-// import axios from "axios";
-// import "./Account.css";
-// import { LoginContext } from "../../components/context/LoginContext";
+import AccountNav from "./AccountNav";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import "./Account.css";
+import { LoginContext } from "../../components/context/LoginContext";
 
 
-// const Logout = () => {
+const Logout = () => {
 
-//     const { setLoggedIn } = useContext(LoginContext);
-//     function loginHandler() {
-//         setLoggedIn(true);
-//     };
+    const { setLoggedIn } = useContext(LoginContext);
+    
+    function loginHandler() {
+        setLoggedIn(false);
+    };
 
-// const url = "http://localhost:3500/logout";
-// const navigate = useNavigate();
+    const url = "http://localhost:3500/logout";
+    const navigate = useNavigate();
 
-// async function logout () {
-//     await axios.post(url)
-//     .then(res => {
-//         loginHandler();
-//         navigate("/");
-//     });
-// };
+    async function logout() {
+        await axios.post(url)
+            .then(res => {
+                loginHandler();
+                navigate("/");
+            });
+    };
 
-// }
+    logout();
 
-// export default Logout;
+}
+
+export default Logout;
